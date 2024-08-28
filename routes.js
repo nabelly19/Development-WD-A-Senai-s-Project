a18682bb13442a4bb0e97633c6292ee5c3c3fade
anabelly <anabellymontibeller@gmail.com>
2024-08-28 10:50:30 -0300

    add in routes

--- routes.js
+++ routes.js
@@ -2,12 +2,20 @@ const express = require('express');
 const routes = express.Router();
 
 const empresa = require('./src/controller/empresaController');
+const login = require('./src/controller/loginController')
 const { authenticateToken } = require('./src/config/auth');  // Importe o middleware de autenticação
 
 routes
-    .post('/add-user', empresa.registrarEmpresa)  // Registro não precisa de autenticação
-    .get('/empresa/:id', authenticateToken, empresa.getEmpresaById)  // Protegido com JWT
-    .delete('/Remove-empresa/:id', authenticateToken, empresa.deleteEmpresaById)  // Protegido com JWT
-    .put('/Update-empresa/:id', authenticateToken, empresa.updateEmpresaById);  // Protegido com JWT
+   // Rota para registro de empresas (não autenticada)
+routes.post('/add-user', empresa.registrarEmpresa);
+
+// Rota para login (não autenticada)
+routes.post('/login', login.login);
+
+// Rotas protegidas (autenticadas)
+routes.get('/empresa/:id', authenticateToken, empresa.getEmpresaById);
+routes.delete('/remove-empresa/:id', authenticateToken, empresa.deleteEmpresaById);
+routes.put('/update-empresa/:id', authenticateToken, empresa.updateEmpresaById);
+
 
 module.exports = routes;
